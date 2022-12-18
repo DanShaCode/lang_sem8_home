08fbcefcc61ef3d5db829397757a953af3e90918
Danis Sharipov <danshadance@mail.ru>
2022-12-18 14:15:35 +0600

    жесткий костыль на 62 задачу

--- task62/task62.cs
+++ task62/task62.cs
@@ -21,11 +21,18 @@ void SnakeFillArr(int[,] arr)
         int i = 0;
         arr[i,j] = num;
         int temp = num;
-        for (int k = 0; k < arr.GetLength(0); k++)
+        for (int k = 0; k < arr.GetLength(0) - 1; k++)
         {
             temp++;
             int l = 3;
             arr[k,l] = temp;
+            int temp1 = temp + 5;
+            for (int x = 0; x < arr.GetLength(1); x++)
+            {
+                temp1--;
+                int y = 3;
+                arr[y,x] = temp1;
+            }
         }
     }
 }
