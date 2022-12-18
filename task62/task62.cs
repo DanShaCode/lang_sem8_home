811f70f2a1b2bd503d60b12be5a4751bb963345b
Danis Sharipov <danshadance@mail.ru>
2022-12-18 18:58:23 +0600

    Отредактировал задачи

--- task62/task62.cs
+++ task62/task62.cs
@@ -1,4 +1,16 @@
-﻿
+﻿// Задача 62. 
+// Создать массив 4 на 4 заполненый нулями.
+// Этот же массив заполнить спирально числами от 1 до 16.
+
+// Например, на выходе получается вот такой массив:
+
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+// Начало программы ==============================================================================
+
 int[,] snake = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
 
 Console.WriteLine();
@@ -27,46 +39,46 @@ void PrintArr(int[,] arr)
 void SnakeFillArr(int[,] arr)
 {
     int num = 0;
-    for (int j = 0; j < arr.GetLength(1) - 1; j++) // от 1 до 4 вправо
+    for (int j = 0; j < arr.GetLength(1) - 1; j++)
     {
         num++;
         int i = 0;
-        arr[i,j] = num;
+        arr[i, j] = num;
         int temp = num;
-        for (int k = 0; k < arr.GetLength(0) - 1; k++) // от 4 до 7 вниз
+        for (int k = 0; k < arr.GetLength(0) - 1; k++)
         {
             temp++;
             int l = 3;
-            arr[k,l] = temp;
+            arr[k, l] = temp;
             int temp1 = temp + 5;
-            for (int x = 0; x < arr.GetLength(1); x++) // от 7 до 10 влево
+            for (int x = 0; x < arr.GetLength(1); x++)
             {
                 temp1--;
                 int y = 3;
-                arr[y,x] = temp1;
+                arr[y, x] = temp1;
                 int temp2 = temp1 + 6;
-                for (int a = 1; a < arr.GetLength(0) - 1; a++) // от 10 до 12 наверх
+                for (int a = 1; a < arr.GetLength(0) - 1; a++)
                 {
                     temp2--;
                     int b = 0;
-                    arr[a,b] = temp2;
+                    arr[a, b] = temp2;
                     int temp3 = temp2 + 1;
-                    for (int z = 1; z < arr.GetLength(1) - 1; z++) // от 12 до 14 вправо
+                    for (int z = 1; z < arr.GetLength(1) - 1; z++)
                     {
                         temp3 = temp3 + 1;
                         int g = 1;
-                        arr[g,z] = temp3;
+                        arr[g, z] = temp3;
                         int temp4 = temp3 - 1;
-                        for (int v = 1; v < arr.GetLength(0) - 1; v++) // от 14 до 15 вниз
+                        for (int v = 1; v < arr.GetLength(0) - 1; v++)
                         {
                             temp4++;
                             int c = 2;
-                            arr[v,c] = temp4;
+                            arr[v, c] = temp4;
                             int temp5 = temp4;
-                            for (int q = 1; q < arr.GetLength(1) - 2;q++) // от 15 до 16 влево
+                            for (int q = 1; q < arr.GetLength(1) - 2; q++)
                             {
                                 int s = 2;
-                                arr[s,q] = temp5 + 1;
+                                arr[s, q] = temp5 + 1;
                             }
                         }
                     }
@@ -75,4 +87,3 @@ void SnakeFillArr(int[,] arr)
         }
     }
 }
-
